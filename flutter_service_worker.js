df90d5c69eb8232c6d2064a94fc15e4922c099c7
zainabaljifri <80160006+zainabaljifri@users.noreply.github.com>
2024-05-29 13:08:09 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -91,9 +91,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "b61a9247d80f78ea2a8126ea96c1f81c",
-"/": "b61a9247d80f78ea2a8126ea96c1f81c",
-"main.dart.js": "4184a156fddc1b2e63a6998db95f6b95",
+"index.html": "8c3166aa09a40e71c1b81e0bab0b3fb4",
+"/": "8c3166aa09a40e71c1b81e0bab0b3fb4",
+"main.dart.js": "23c77c60ca154594d275b7c23d73eb42",
 "manifest.json": "3f92a849556967510ecd50602f08bc15",
 "version.json": "5e129f52952a920d64681e367187e4ef"
 };
